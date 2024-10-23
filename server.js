341ad0199598487898f4a745c34237e27a4530b6
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-10-23 04:12:09 +0200

    added first graphql query and mutation to query user or users and to add a user

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDB = require("./config");
+const { graphqlHTTP } = require("express-graphql");
+const userSchema = require("./GQLSchemas/userSchema");
 
 connectDB();
 
-const port = 443 || process.env.PORT;
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: userSchema,
+    graphiql: true,
+  })
+);
+
+const port = process.env.PORT || 443;
 app.listen(port, () => console.log(`server running on ${port}`));
